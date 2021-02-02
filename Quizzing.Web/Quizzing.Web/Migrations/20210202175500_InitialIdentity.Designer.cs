d3b35c439ea4b29260b0fbfd3c9adab32bdf331d
Joana Boiso-Ayan <J.Ayan@ucas.ac.uk>
2021-02-02 18:01:00 +0000

    Identity tables up

--- Quizzing.Web/Quizzing.Web/Migrations/20210202175500_InitialIdentity.Designer.cs
+++ Quizzing.Web/Quizzing.Web/Migrations/20210202175500_InitialIdentity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Quizzing.Web.Data;
@@ -9,9 +10,10 @@ using Quizzing.Web.Data;
 namespace Quizzing.Web.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210202175500_InitialIdentity")]
+    partial class InitialIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
